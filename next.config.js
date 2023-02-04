c9a024555d4604931207875b2493e50057235ace
luis Carlos Jarero Martinez <jareroluis@gmail.com>
2023-02-04 01:03:25 -0600

    feature/initial-configure-app

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  reactStrictMode: true,
+  env: {
+    PROJECT_KEY: process.env.PROJECT_KEY,
+    CLIENT_ID: process.env.CLIENT_ID,
+    SECRET: process.env.SECRET,
+    API_URL: process.env.API_URL,
+    AUTH_URL: process.env.AUTH_URL,
+    ENCODED_BASIC_AUTH: process.env.ENCODED_BASIC_AUTH
+  },
+  images: {
+    domains: ['s3-eu-west-1.amazonaws.com'],
+  },
+  fontLoaders: [
+    { loader: "@next/font/google", options: { subsets: ["latin"] } },
+  ],
 }
 
 module.exports = nextConfig
